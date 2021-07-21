8ce28aa5c546cf9e10a603d7d6f85131f3d8a133
Rigoberto Contreras <rigoc1994@gmail.com>
2021-07-21 18:04:09 -0400

    first

---
+++ index.js
@@ -0,0 +1,25 @@
+const express  = require("express")
+// load variables
+const  env =  require("./config/env.json")
+const colors = require("colors");
+const morgan = require("morgan");
+const app =  express();
+
+
+
+//middleware
+app.use(express.urlencoded(),express.json())
+app.use(morgan());
+
+//load routes
+
+//setup routes
+
+
+
+//Listen to app
+const PORT =  env.PORT || 5001;
+
+app.listen(PORT,()=>{
+       console.log(`listening on port ${PORT}`.green)
+});
---
+++ routes/files.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const  Router =  express.Router();
+
+const { } =  require("")
+
+Router
+    .route('/')
+        .get()
+
+
+module.exports =  Router;
